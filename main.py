af5a75bce2220d5b68819081e6d60d05a1d9570c
shhanInD <shhan@wiselycompany.com>
2022-11-09 03:39:50 +0900

    v1-importrange 함수 내 range 편의성개선

--- main.py
+++ main.py
@@ -4,9 +4,17 @@ from utils import get_gs_structure
 import time
 import re
 import string
+from datetime import datetime
 
+global sec
+
+start_ = time.time()
 # 구글시트 내 필요한 시트의 url, id 가져오기
 structure = get_gs_structure()
+
+print("준비 끝")
+lap = time.time()-start_
+print("준비까지 : ", lap, "초")
 
 def importrange(copy_ssname, copy_wsname, paste_ssname, paste_wsname, range,
                  paste = "A1", structure = structure,
@@ -45,7 +53,12 @@ def importrange(copy_ssname, copy_wsname, paste_ssname, paste_wsname, range,
     def celladdress_breakdown(celladdress):
         row = re.sub(r'[^0-9]', '', celladdress)
         col = col2num(celladdress.replace(row,""))
-        return int(row), int(col)
+        if row == "": row = None
+        else : row = int(row)
+        if col == "": col = None
+        else : col = int(col)
+
+        return row, col
 
     default_gsurl = 'https://docs.google.com/spreadsheets/d/'
     gs = GoogleSheet()
@@ -109,10 +122,61 @@ def importrange(copy_ssname, copy_wsname, paste_ssname, paste_wsname, range,
                        row=paste_fromrow_py,
                        col=paste_fromcolumn_py)
 
-# AOQ_RAW(AOQ_by_seg) -> 주문량 예측 Promo(AOQ_by_seg)
-# importrange(copy_ssname = "AOQ_RAW", copy_wsname= "AOQ_by_seg", paste_ssname="주문량 예측 Promo", paste_wsname="AOQ_by_seg(importrange)", rows_between=[2,14])
-# time.sleep(1.5)
-# importrange(copy_ssname = "AOQ_RAW", copy_wsname= "AOQ_firstRe", paste_ssname="주문량 예측 Promo", paste_wsname="AOQ_firstRe(importrange)", rows_between=[2,4])
-# time.sleep(1.5)
-importrange(copy_ssname="수요예측 주문량", copy_wsname="Onetime_N1N2_RR", paste_ssname="주문량 예측 Promo", paste_wsname="Promo Reference", range=["J3","J368"], paste="C10", transpose=True )
-# AOQ_RAW(AOQ_firstRe) -> 주문량 예측 Promo(AOQ_firstRe)
+def work():
+    ## AOQ_RAW - > 주문량 예측 Promo
+    start = time.time()
+    importrange(copy_ssname = "AOQ_RAW", copy_wsname= "AOQ_firstRe", paste_ssname="주문량 예측 Promo", paste_wsname="AOQ_firstRe(importrange)", range=["2","4"], paste = "A1")
+    lap_1 = time.time()-start
+    sec = "AOQ_RAW - > 주문량 예측 Promo - AOQ_firstRe(importrange)"
+    #print("AOQ_RAW - > 주문량 예측 Promo - AOQ_firstRe(importrange) : ", lap_1, "초")
+
+    # start = time.time()
+    # importrange(copy_ssname="수요예측 주문량", copy_wsname="Onetime_N1N2_RR", paste_ssname="주문량 예측 Promo", paste_wsname="Promo Reference", range=["J3","J368"], paste="C10", transpose=True )
+    # lap = time.time()-start
+    # print("복사 2 : ", lap, "초")
+
+    ## 주석풀면됨
+    ## '주문량 예측 시트'에 정보 넣기
+    start = time.time()
+    importrange(copy_ssname="주문량 예측 Promo", copy_wsname="Promo Plan (UA)", paste_ssname="주문량 예측", paste_wsname="신규유입자 계산", range=["A2","D7"], paste="A1")
+    lap_2 = time.time()-start
+    sec = "주문량 예측 Promo - > 주문량 예측 - 신규유입자 계산 _ 1"
+    #print("주문량 예측 Promo - > 주문량 예측 - 신규유입자 계산 _ 1 : ", lap_2, "초")
+
+    start = time.time()
+    importrange(copy_ssname="주문량 예측 Promo", copy_wsname="Promo Reference", paste_ssname="주문량 예측", paste_wsname="신규유입자 계산", range=["B3","C3"], paste="H1", transpose = True)
+    lap_3 = time.time()-start
+    sec = "주문량 예측 Promo - > 주문량 예측 - 신규유입자 계산 _ 2"
+    #print("주문량 예측 Promo - > 주문량 예측 - 신규유입자 계산 _ 2 : ", lap_3, "초")
+
+    start = time.time()
+    importrange(copy_ssname="주문량 예측 Promo", copy_wsname="Promo 첫전환 주문량 예측", range=["NJ2","ABK7"], paste_ssname="주문량 예측", paste_wsname="주문량 예측 시트", paste="B23")
+    lap_4 = time.time()-start
+    sec = "주문량 예측 Promo - > 주문량 예측 - 주문량 예측 시트 _ 1"
+    #print("주문량 예측 Promo - > 주문량 예측 - 주문량 예측 시트 _ 1 : ", lap_4, "초")
+
+    start = time.time()
+    importrange(copy_ssname="주문량 예측 Promo", copy_wsname="Re_Promo_주문량_예측", range=["E2","NF2"], paste_ssname="주문량 예측", paste_wsname="주문량 예측 시트", paste="B36")
+    lap_5 = time.time()-start
+    sec = "주문량 예측 Promo - > 주문량 예측 - 주문량 예측 시트 _ 2"
+    #print("주문량 예측 Promo - > 주문량 예측 - 주문량 예측 시트 _ 2 : ", lap_5, "초")
+
+    start = time.time()
+    ## 'SKU별 수요량 예측'시트에 정보 넣기
+    importrange(copy_ssname="주문량 예측", copy_wsname="주문량 예측 시트", range=["A","NC"], paste_ssname="SKU별 수요량 예측", paste_wsname="주문량 예측", paste="A1")
+    lap_6 = time.time()-start
+    sec = "주문량 예측 Promo - > SKU별 수요량 예측 - 주문량 예측"
+    #print("주문량 예측 Promo - > SKU별 수요량 예측 - 주문량 예측 : ", lap_6, "초")
+
+    print("복사 끝")
+    lap_fin = time.time()-start_
+    #print("총 시간 : ", lap_fin, "초")
+
+    return [lap_1, lap_2, lap_3, lap_4, lap_5, lap_6, sec]
+
+try:
+    print("************************"datetime.now().date(), datetime.now().time(), "함수 실행************************")
+    worktime = work()
+    print(worktime[-1], "초 걸려 작업 완료")
+except Exception as e:
+    print("==============", sec, "에서 문제가 생겼습니다.==============")
